9769cf0ebbddad063aebdf8152eedfe9d4227e9a
Michael <Michael@Home.home>
2019-12-07 20:14:28 -0500

    dev check-in. still work to do

--- Game.py
+++ Game.py
@@ -1,145 +1,121 @@
 import tkinter as tk
 from level import *
 
-root = tk.Tk()
-root.attributes("-fullscreen", True)
-bg_color='#dccdc7'
-root.configure(background=bg_color)
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-button_width=400
-button_height=50
-root.button_width=400
-root.button_height=50
+class Game:
+	def __init__(self):
+		self.init_globals()
+		#grade4_levels(self.root)
+		main_menu_args = (("Grade 1", self.level4), ("Grade 2", self.level4), ("Grade 3", self.level4),
+					  ("Grade 4", self.level4_valid), (None), ("About", "red", self.about), ("Exit Game", "red", quit))
+		self.main_menu_frame = menu_buttons(self.root, "ArithMagic!", *main_menu_args)
+		self.all_frames = [self.main_menu_frame]
+		self.main_menu_frame.show()
+		tk.mainloop()
 
-menu_widgets = []
-about_widgets = []
-level4_widgets = []
+	def init_globals(self):
+		self.root = tk.Tk()
+		self.root.attributes("-fullscreen", True)
+		self.root.bg_color='#dccdc7'
+		self.root.configure(background=self.root.bg_color)
+		self.screen_width = self.root.winfo_screenwidth()
+		self.screen_height = self.root.winfo_screenheight()
+		self.root.button_width=400
+		self.root.button_height=50
 
-about_text = "Developed by Michael Santana, Justin Dom, Liliana Byan, Michael Batbouta"
-score = 0
-stopwatch = 0
-score_var_obj = tk.StringVar()
-score_var_obj.set('Score: ' + str(score))
-watch_var_obj = tk.StringVar()
-watch_var_obj.set('time: ' + str(stopwatch) + ' seconds')
-playing = False
+		self.menu_widgets = []
+		self.about_widgets = []
+		self.level4_widgets = []
 
-def game_title():
-	global menu_widgets
-	w = tk.Label(root, text="ArithMagic!", font="Times 72 italic", fg="red", bg=bg_color)
-	w.place(x=-100000, y=-100000)
-	w.update() #stupid library wont give the actual width until you first call place and update
-	label_width = w.winfo_width()
-	w.place(x=screen_width/2 - label_width/2, y=screen_height/6)
-	menu_widgets.append(w)
+		self.about_text = "Developed by Michael Santana, Justin Dom, Liliana Byan, Michael Batbouta"
+		self.score = 0
+		self.stopwatch = 0
+		self.score_var_obj = tk.StringVar()
+		self.score_var_obj.set('Score: ' + str(self.score))
+		self.watch_var_obj = tk.StringVar()
+		self.watch_var_obj.set('time: ' + str(self.stopwatch) + ' seconds')
+		self.playing = False
 
-def show_main():
-	global playing
-	for aw in about_widgets:
-		aw.pi = aw.place_info()
-		aw.place_forget()
-	for l4w in level4_widgets:
-		l4w.pi = l4w.place_info()
-		l4w.place_forget()
-	for mw in menu_widgets:
-		mw.place(mw.pi)
-	playing = False
+	def hide_all(self):
+		for aw in self.about_widgets:
+			aw.pi = aw.place_info()
+			aw.place_forget()
+		for l4w in self.level4_widgets:
+			l4w.pi = l4w.place_info()
+			l4w.place_forget()
+		for frame in self.all_frames:
+			frame.hide()
 
-def about():
-	global about_widgets
-	for w in menu_widgets:
-		w.pi = w.place_info()
-		w.place_forget()
+	def show_main(self):
+		self.hide_all()
+		self.main_menu_frame.show()
+		self.playing = False
+	
+	def about(self):
+		self.hide_all()
 
-	w1 = tk.Label(root, text="About", font="Times 72", fg="red", bg=bg_color)
-	w1.place(x=-100000, y=-100000)
-	w1.update() #stupid library wont give the actual width until you first call place and update
-	label_width = w1.winfo_width()
-	w1.place(x=screen_width/2 - label_width/2, y=screen_height/6)
-	w2 = tk.Button(root, text="Back", bg="red", font="Times 32", fg="white", command=show_main)
-	w2.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*6.25, width=button_width, height=button_height)
-	w3 = tk.Label(root, text=about_text, font="Times 16", fg="black", bg=bg_color)
-	w3.place(x=screen_width/2 - button_width, y=screen_height/2 - button_height*1.25, width=button_width*2, height=button_height)
-	about_widgets = [w1, w2, w3]
+		w1 = tk.Label(self.root, text="About", font="Times 72", fg="red", bg=self.root.bg_color)
+		w1.place(x=-100000, y=-100000)
+		w1.update() #stupid library wont give the actual width until you first call place and update
+		label_width = w1.winfo_width()
+		w1.place(x=self.screen_width/2 - label_width/2, y=self.screen_height/6)
+		w2 = tk.Button(self.root, text="Back", bg="red", font="Times 32", fg="white", command=self.show_main)
+		w2.place(x=self.screen_width/2 - self.root.button_width/2, y=self.screen_height/2 + self.root.button_height*6.25, width=self.root.button_width, height=self.root.button_height)
+		w3 = tk.Label(self.root, text=self.about_text, font="Times 16", fg="black", bg=self.root.bg_color)
+		w3.place(x=self.screen_width/2 - self.root.button_width, y=self.screen_height/2 - self.root.button_height*1.25, width=self.root.button_width*2, height=self.root.button_height)
+		self.about_widgets = [w1, w2, w3]
+	
+	def correct(self):
+		self.score = self.score + 10
+		self.score_var_obj.set('Score: ' + str(self.score))
+	
+	def wrong(self):
+		self.score = self.score - 5
+		if self.score < 0:
+			self.score = 0
+		self.score_var_obj.set('Score: ' + str(self.score))
+	
+	def update_watch(self):
+		if self.playing:
+			self.stopwatch = self.stopwatch + 1
+			self.watch_var_obj.set('time: ' + str(self.stopwatch) + ' seconds')
+			self.root.after(1000, self.update_watch)
+	
+	def level4(self):
+		pass
 
-def correct():
-	global score
-	global score_var_obj
-	score = score + 10
-	score_var_obj.set('Score: ' + str(score))
+	def level4_valid(self):
+		self.main_menu_frame.hide()
+		grade4_levels(self.root, self.main_menu_frame)
 
-def wrong():
-	global score
-	global score_var_obj
-	score = score - 5
-	if score < 0:
-		score = 0
-	score_var_obj.set('Score: ' + str(score))
-
-def update_watch():
-	if playing:
-		global stopwatch
-		stopwatch = stopwatch + 1
-		global watch_var_obj
-		watch_var_obj.set('time: ' + str(stopwatch) + ' seconds')
-		root.after(1000, update_watch)
-
-def level4(): #TODO this was a big copy paste from the start_menu_buttons. make sure everything is correct
-	global playing
-	playing = True
-	global stopwatch
-	stopwatch = 0
-	global level4_widgets
-	for w in menu_widgets:
-		w.pi = w.place_info()
-		w.place_forget()
-	w0 = tk.Label(root, text="What is the next number in the sequence?\n3, 6, 9, 12, 15", font="Times 48", fg="red", bg=bg_color)
-	w0.place(x=-100000, y=-100000)
-	w0.update() #stupid library wont give the actual width until you first call place and update
-	label_width = w0.winfo_width()
-	w0.place(x=screen_width/2 - label_width/2, y=screen_height/6)
-	w1 = tk.Radiobutton(root, text="5", bg="blue", font="Times 32", fg="white", command=wrong)
-	w1.place(x=screen_width/2 - button_width/2, y=screen_height/2 - button_height*1.25, width=button_width, height=button_height)
-	w2 = tk.Radiobutton(root, text="20", bg="blue", font="Times 32", fg="white", command=wrong)
-	w2.place(x=screen_width/2 - button_width/2, y=screen_height/2, width=button_width, height=button_height)
-	w3 = tk.Radiobutton(root, text="18", bg="blue", font="Times 32", fg="white", command=correct)
-	w3.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*1.25, width=button_width, height=button_height)
-	w4 = tk.Radiobutton(root, text="17", bg="blue", font="Times 32", fg="white", command=wrong)
-	w4.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*2.5, width=button_width, height=button_height)
-	w5 = tk.Button(root, text="Back", bg="red", font="Times 32", fg="white", command=show_main)
-	w5.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*6.25, width=button_width, height=button_height)
-	w6 = tk.Label(root, textvariable=score_var_obj, bg="red", font="Times 32", fg="white")
-	w6.place(x=0, y=0, width=button_width, height=button_height)
-	w7 = tk.Label(root, textvariable=watch_var_obj, bg="red", font="Times 32", fg="white")
-	w7.place(x=0, y=button_height, width=button_width, height=button_height)
-
-	root.after(1000, update_watch)
-	level4_widgets = [w0, w1, w2, w3, w4, w5, w6, w7]
-
-def start_menu_buttons():
-	global menu_widgets
-	w1 = tk.Button(root, text="Grade 1", bg="blue", font="Times 32", fg="white", command=level4)
-	w1.place(x=screen_width/2 - button_width/2, y=screen_height/2 - button_height*1.25, width=button_width, height=button_height)
-	w2 = tk.Button(root, text="Grade 2", bg="blue", font="Times 32", fg="white", command=level4)
-	w2.place(x=screen_width/2 - button_width/2, y=screen_height/2, width=button_width, height=button_height)
-	w3 = tk.Button(root, text="Grade 3", bg="blue", font="Times 32", fg="white", command=level4)
-	w3.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*1.25, width=button_width, height=button_height)
-	w4 = tk.Button(root, text="Grade 4", bg="blue", font="Times 32", fg="white", command=level4)
-	w4.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*2.5, width=button_width, height=button_height)
-	w5 = tk.Button(root, text="About", bg="red", font="Times 32",fg="white", command=about)
-	w5.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*5, width=button_width, height=button_height)
-	w6 = tk.Button(root, text="Exit Game", bg="red", font="Times 32", fg="white", command=quit)
-	w6.place(x=screen_width/2 - button_width/2, y=screen_height/2 + button_height*6.25, width=button_width, height=button_height)
-	menu_widgets.extend([w1,w2,w3,w4,w5,w6])
+	def level4t(self): #TODO this was a big copy paste from the start_menu_buttons. make sure everything is correct
+		self.playing = True
+		self.stopwatch = 0
+		self.hide_all()
+		w0 = tk.Label(self.root, text="What is the next number in the sequence?\n3, 6, 9, 12, 15", font="Times 48", fg="red", bg=self.root.bg_color)
+		w0.place(x=-100000, y=-100000)
+		w0.update() #stupid library wont give the actual width until you first call place and update
+		label_width = w0.winfo_width()
+		w0.place(x=self.screen_width/2 - label_width/2, y=self.screen_height/6)
+		w1 = tk.Radiobutton(self.root, text="5", bg="blue", font="Times 32", fg="white", command=self.wrong)
+		w1.place(x=self.screen_width/2 - self.root.button_width/2, y=self.screen_height/2 - self.root.button_height*1.25, width=self.root.button_width, height=self.root.button_height)
+		w2 = tk.Radiobutton(self.root, text="20", bg="blue", font="Times 32", fg="white", command=self.wrong)
+		w2.place(x=self.screen_width/2 - self.root.button_width/2, y=self.screen_height/2, width=self.root.button_width, height=self.root.button_height)
+		w3 = tk.Radiobutton(self.root, text="18", bg="blue", font="Times 32", fg="white", command=self.correct)
+		w3.place(x=self.screen_width/2 - self.root.button_width/2, y=self.screen_height/2 + self.root.button_height*1.25, width=self.root.button_width, height=self.root.button_height)
+		w4 = tk.Radiobutton(self.root, text="17", bg="blue", font="Times 32", fg="white", command=self.wrong)
+		w4.place(x=self.screen_width/2 - self.root.button_width/2, y=self.screen_height/2 + self.root.button_height*2.5, width=self.root.button_width, height=self.root.button_height)
+		w5 = tk.Button(self.root, text="Back", bg="red", font="Times 32", fg="white", command=self.show_main)
+		w5.place(x=self.screen_width/2 - self.root.button_width/2, y=self.screen_height/2 + self.root.button_height*6.25, width=self.root.button_width, height=self.root.button_height)
+		w6 = tk.Label(self.root, textvariable=self.score_var_obj, bg="red", font="Times 32", fg="white")
+		w6.place(x=0, y=0, width=self.root.button_width, height=self.root.button_height)
+		w7 = tk.Label(self.root, textvariable=self.watch_var_obj, bg="red", font="Times 32", fg="white")
+		w7.place(x=0, y=self.root.button_height, width=self.root.button_width, height=self.root.button_height)
+	
+		self.root.after(1000, self.update_watch)
+		self.level4_widgets = [w0, w1, w2, w3, w4, w5, w6, w7]
 
 def main():
-	game_title()
-	#start_menu_buttons()
-	main_menu_args = (("Grade 1", level4), ("Grade 2", level4), ("Grade 3", level4),
-					  ("Grade 4", level4), (None), ("About", "red", about), ("Exit Game", "red", quit))
-	mylevel = menu_buttons(root, *main_menu_args)
-	tk.mainloop()
+	game = Game()
 
 if __name__=="__main__":
     main()
--- level.py
+++ level.py
@@ -6,18 +6,21 @@ class frame:
 		self.widgets = []
 		self.root = root
 		self.hidden = False
+		self.screen_width = self.root.winfo_screenwidth()
+		self.screen_height = self.root.winfo_screenheight()
 
 	def hide(self):
 		for w in self.widgets:
+			if not w.place_info():
+				continue # widget is already hidden
 			w.pi = w.place_info()
 			w.place_forget()
 
 	def show(self):
 		for w in self.widgets:
-			w.pi = w.place(w.pi)
+			w.place(w.pi)
 
 	def toggle(self):
-		print(self)
 		if self.hidden:
 			self.show()
 			self.hidden = False
@@ -26,22 +29,61 @@ class frame:
 			self.hidden = True
 
 class choice(frame): #TODO
-	def __init__(self, root):
+	def __init__(self, frame_arr, root, question, answer1, answer2, answer3, answer4, correct):
 		frame.__init__(self, root)
-		self.w6 = tk.Button(self.root, text="Exit Game", bg="red", font="Times 32", fg="white")
-		self.w6.place(x=200, y=200, width=200, height=100)
-		self.w7 = tk.Button(self.root, text="Toggle", bg="red", font="Times 32", fg="white", command=self.toggle)
-		self.w7.place(x=400, y=400, width=200, height=100)
-		self.widgets.append(self.w6)
-		print("hello there!")
+		self.append_self(frame_arr)
+		w0 = tk.Label(root, text=question, font="Times 48", fg="red", bg=root.bg_color)
+		w0.place(x=-100000, y=-100000)
+		w0.update() #stupid library wont give the actual width until you first call place and update
+		label_width = w0.winfo_width()
+		w0.place(x=self.screen_width/2 - label_width/2, y=self.screen_height/6)
+		self.widgets.append(w0)
+		self.var = -1
+		self.i = 0
+		self.default_button(answer1, -self.screen_width/8, -self.root.button_height*2.5)
+		self.default_button(answer2, self.screen_width/8, -self.root.button_height*2.5)
+		self.default_button(answer3, -self.screen_width/8, self.root.button_height*1.25)
+		self.default_button(answer4, self.screen_width/8, self.root.button_height*1.25)
+		self.default_button("next", self.screen_width/2 -self.root.button_width/2, self.screen_height/2 - 3*self.root.button_height/2, 0)
+		self.hide()
+
+	def default_button(self, txt, xpos, ypos, button_type=1):
+		if button_type:
+			w = tk.Radiobutton(self.root, text=txt, activebackground="khaki", bg="blue", 
+				font="Times 32", #fg="white", 
+				indicatoron=0, variable=self.var, value=self.i)
+		else:
+			w = tk.Button(self.root, text=txt, activebackground="khaki", bg="red", 
+				font="Times 32", command=self.next_frame)
+
+		w.place(x=self.screen_width/2 - self.root.button_width/2 + xpos, y=self.screen_height/2 - self.root.button_height/2 + ypos, width=self.root.button_width, height=self.root.button_height)
+		self.widgets.append(w)
+		self.i = self.i + 1
+		return w
+
+	def append_self(self, frame_arr):
+		self.frame_arr = frame_arr
+		self.frame_arr.append(self)
+
+	def next_frame(self):
+		if len(self.frame_arr) <= 1:
+			print("If you see this message it means you dont have a callback frame and you are probably seeing an empty screen")
+		self.hide()
+		self.frame_arr.pop()
+		if len(self.frame_arr) > 0:
+			self.frame_arr[len(self.frame_arr)-1].show()
 
 class menu_buttons(frame): #This is a generic class use to create main manu and other frames similar to the main menu
-	def __init__(self, root, *argv):
+	def __init__(self, root, title, *argv):
 		frame.__init__(self, root)
-		screen_width = root.winfo_screenwidth()
-		screen_height = root.winfo_screenheight()
 		i = -1.25
-		print(len(argv))
+		w = tk.Label(self.root, text=title, font="Times 72 italic", fg="red", bg=self.root.bg_color)
+		w.place(x=-100000, y=-100000)
+		w.update() #stupid library wont give the actual width until you first call place and update
+		label_width = w.winfo_width()
+		w.place(x=self.screen_width/2 - label_width/2, y=self.screen_height/6)
+		self.widgets.append(w)
+
 		for arg in argv:
 			if arg is not None:
 				button_str = arg[0]
@@ -61,7 +103,7 @@ class menu_buttons(frame): #This is a generic class use to create main manu and 
 						print("I dont know what type this is. should have been a string for color or a callback function. exiting.")
 						sys.exit(0)
 				elif len(arg) == 3:
-					if type(arg[1]) is not str or not callable(arg[2]):	
+					if type(arg[1]) is not str or not callable(arg[2]):
 						print("I dont know what type this is. should have been a string for color and a callback function. exiting.")
 						sys.exit(0)
 					color = arg[1]
@@ -70,10 +112,19 @@ class menu_buttons(frame): #This is a generic class use to create main manu and 
 					print("I dont know what type this is.")
 					sys.exit(0)
 				w = tk.Button(root, text=button_str, bg=color, font="Times 32", fg="white", command=fun)
-				w.place(x=screen_width/2 - root.button_width/2, y=screen_height/2 + root.button_height*i, width=root.button_width, height=root.button_height)
+				w.place(x=self.screen_width/2 - root.button_width/2, y=self.screen_height/2 + root.button_height*i, width=root.button_width, height=root.button_height)
 				self.widgets.append(w)
-			print("i: " + str(i))
-			print("height pos: " + str(screen_height/2 - root.button_height*i))
-			print("screen_height: " + str(screen_height) + " root.button_height: " + str(root.button_height))
 			i = i + 1.25
-		#self.hide()
+		self.hide()
+
+class grade4_levels():
+	def __init__(self, root, callback_frame):
+		self.frame_arr = []
+		self.frame_arr.append(callback_frame)
+		grade4 = choice(self.frame_arr, root, "What is the next number in the sequence?\n3, 6, 9, 12, 15", "5", "20", "18", "17", 3)
+		choice(self.frame_arr, root, "What is the next number in the sequence?\n3, 6, 9, 12, 15", "5", "30", "18", "13", 3)
+		choice(self.frame_arr, root, "What is the next number in the sequence?\n3, 6, 9, 12, 15", "7", "21", "18", "15", 3)
+		choice(self.frame_arr, root, "What is the next number in the sequence?\n3, 6, 9, 12, 15", "9", "23", "18", "19", 3)
+		first = choice(self.frame_arr, root, "What is the next number in the sequence?\n3, 6, 9, 12, 15", "3", "25", "18", "14", 3)
+		first.show()
+
